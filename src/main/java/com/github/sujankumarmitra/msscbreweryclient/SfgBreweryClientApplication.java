8d48333ded8cd0299d8bf56c72a3466476bca17a
skmitra <mitrakumarsujan@gmail.com>
2021-01-05 19:54:53 +0530

    Project Init

---
+++ src/main/java/com/github/sujankumarmitra/msscbreweryclient/SfgBreweryClientApplication.java
@@ -0,0 +1,13 @@
+package com.github.sujankumarmitra.msscbreweryclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgBreweryClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgBreweryClientApplication.class, args);
+	}
+
+}
